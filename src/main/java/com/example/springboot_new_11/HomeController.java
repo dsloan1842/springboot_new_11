1b6765960f0397ac1cb29f6771ca4e09f757e832
RVSW317007DT <RVSW317007DT@MC.MONTGOMERYCOLLEGE.EDU>
2017-08-23 11:17:40 -0400

    Initial commit

---
+++ src/main/java/com/example/springboot_new_11/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.springboot_new_11;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
---
+++ src/main/java/com/example/springboot_new_11/SpringbootNew11Application.java
@@ -0,0 +1,12 @@
+package com.example.springboot_new_11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootNew11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootNew11Application.class, args);
+	}
+}
